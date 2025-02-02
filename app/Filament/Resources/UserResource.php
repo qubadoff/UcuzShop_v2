d5e573f95c910c7eead815a228b27f146841b72a
qubadoff <admin@burncode.org>
2025-02-02 18:22:19 +0400

    new

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -57,7 +57,9 @@ class UserResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
