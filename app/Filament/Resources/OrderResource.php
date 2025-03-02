63170039698256d88ee509ae04b182e435b4fef0
qubadoff <admin@burncode.org>
2025-03-02 06:18:53 +0400

    new

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -85,7 +85,7 @@ class OrderResource extends Resource
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
-                PrintComponent::make("print_page")->label("Print Page")->columnSpanFull(),
+                PrintComponent::make("orders")->label("Print Page")->columnSpanFull(),
         ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
