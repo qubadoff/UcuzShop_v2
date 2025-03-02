5e1ff9186ec4ab8054d682302b95e54fb2cda819
qubadoff <admin@burncode.org>
2025-03-02 06:01:13 +0400

    new

--- app/Http/Controllers/api/v1/Partner/PartnerController.php
+++ app/Http/Controllers/api/v1/Partner/PartnerController.php
@@ -2,23 +2,47 @@
 
 namespace App\Http\Controllers\api\v1\Partner;
 
+use App\Enum\Partner\PartnerStatusEnum;
 use App\Http\Controllers\Controller;
-use App\Http\Resources\Partner\PartnerResource;
+use App\Models\Customer;
+use App\Models\Partner;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class PartnerController extends Controller
 {
     public function __construct(){}
 
-    public function login(Request $request): PartnerResource
+    public function login(Request $request): JsonResponse
     {
         $request->validate([
-            'phone' => 'required|numeric|exists:partners,phone',
+            'phone' => 'required|numeric',
             'password' => 'required|string',
         ]);
 
-        $partner = auth('partner')->attempt($request->only('phone', 'password'));
+        $user = Partner::query()->where('phone', $request->phone)->first();
 
-        return new PartnerResource($partner);
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials.',
+            ], 422);
+        }
+
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        return response()->json([
+            'token' => $token,
+            'partner' => [
+                'name' => $user->name,
+                'country_code' => $user->country_code,
+                'phone' => $user->phone,
+            ],
+            'status' => [
+                'id' => $user->status,
+                'name' => PartnerStatusEnum::tryFrom($user->status)->getLabel(),
+            ]
+        ]);
     }
+
 }
