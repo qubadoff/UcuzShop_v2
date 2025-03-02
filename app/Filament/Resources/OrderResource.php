190deca26553b63a540617b11d831ab070b74a1f
qubadoff <admin@burncode.org>
2025-03-02 06:28:56 +0400

    new

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -73,8 +73,9 @@ class OrderResource extends Resource
                 Tables\Columns\TextColumn::make('customer.name')->searchable(),
                 Tables\Columns\TextColumn::make('price')->sortable()->money(' AZN'),
                 Tables\Columns\TextColumn::make('discount')->sortable()->suffix(' %'),
-                Tables\Columns\ImageColumn::make('orderProduct.product.images')->label('Şəkli'),
-                Tables\Columns\TextColumn::make('status')->badge(),
+                Tables\Columns\ImageColumn::make('orderProduct.product.images')
+                    ->label('Şəkli')
+                    ->getStateUsing(fn ($record) => json_decode($record->orderProduct->product->images, true)[0] ?? 'default.jpg'),                Tables\Columns\TextColumn::make('status')->badge(),
                 Tables\Columns\TextColumn::make('created_at')->dateTime(),
             ])->defaultSort('created_at', 'desc')
             ->filters([
