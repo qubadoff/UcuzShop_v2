f0098de4f85ea2e1763151b07031c054b0d486ef
qubadoff <admin@burncode.org>
2025-03-02 06:05:22 +0400

    new

--- app/Http/Resources/Partner/PartnerResource.php
+++ app/Http/Resources/Partner/PartnerResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Partner;
 
+use App\Enum\Partner\PartnerStatusEnum;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,6 +15,15 @@ class PartnerResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'country_code' => $this->country_code,
+            'phone' => $this->phone,
+            'status' => [
+                'status' => $this->status,
+                'name' => PartnerStatusEnum::tryFrom($this->status->value)->getLabel(),
+            ],
+        ];
     }
 }
