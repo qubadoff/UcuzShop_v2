6679de23d91df3fdb96c80af850ab556df2eefee
qubadoff <admin@burncode.org>
2025-03-14 03:12:40 +0400

    new

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -91,7 +91,7 @@ class OrderResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
-                //Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
